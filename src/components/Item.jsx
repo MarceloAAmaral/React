76fdc6225900c1327f37822968dec5bf9387cc22
MarceloAAmaral <marceloamaralfpolis@gmail.com>
2025-01-22 10:17:34 -0300

    Inicio das atualizações

---
+++ src/components/Item.jsx
@@ -0,0 +1,24 @@
+function Item({item, selectProduct}) {
+    return (
+        <>
+          <div onClick={()=>selectProduct(item.id)} className={`product ${item.isInBag ? 'selected' : ''}`}>
+                        <div className="photo">
+                            <img src={"./img/" + item.photo} />
+                        </div>
+                        <div className="description">
+                            <span className="name">{item.name}</span>
+                            <span className="price">{item.price}</span>
+                            {    
+                                item.isInbag &&
+                                <div className="quantity-area">
+                                     <button>-</button>
+                                    <span className="quantity">{item.quantity}</span>
+                                    <button>+</button>
+                                </div>
+                            }    
+                        </div>
+                    </div>
+        </>
+    )
+}
+export default Item
